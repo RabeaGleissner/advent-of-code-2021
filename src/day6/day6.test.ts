2f747de5fc26f9dffefafd0ea1ec701da32d93aa
Rabea Gleissner <rabeagleissner@gmail.com>
2021-12-07 20:47:24 +0800

    Day 6 part 2

--- src/day6/day6.test.ts
+++ src/day6/day6.test.ts
@@ -1,4 +1,10 @@
-import { nextState, numberOfFish } from "./day6";
+import {
+  convertToPopulation,
+  nextPopulation,
+  nextState,
+  numberOfFishPart1,
+  numberOfFishPart2,
+} from "./day6";
 import input from "./day6input";
 
 describe("lantern fish life", () => {
@@ -18,10 +24,93 @@ describe("lantern fish life", () => {
   });
 
   it("calculates the number of fish after 80 days", () => {
-    expect(numberOfFish([3, 4, 3, 1, 2], 80)).toBe(5934);
+    expect(numberOfFishPart1([3, 4, 3, 1, 2], 80)).toBe(5934);
   });
 
   it("the solution for 80 days", () => {
-    expect(numberOfFish(input, 80)).toBe(375482);
+    expect(numberOfFishPart1(input, 80)).toBe(375482);
+  });
+});
+
+describe("the population of lantern fish", () => {
+  it("converts input to population", () => {
+    const testInput = [
+      0, 1, 0, 5, 6, 0, 1, 2, 2, 3, 0, 1, 2, 2, 2, 3, 3, 4, 4, 5, 7, 8,
+    ];
+
+    expect(convertToPopulation(testInput)).toStrictEqual({
+      0: 4,
+      1: 3,
+      2: 5,
+      3: 3,
+      4: 2,
+      5: 2,
+      6: 1,
+      7: 1,
+      8: 1,
+    });
+  });
+
+  it("finds the next population state without newborns", () => {
+    const currentPopulationState = {
+      0: 0,
+      1: 1,
+      2: 1,
+      3: 2,
+      4: 1,
+      5: 0,
+      6: 0,
+      7: 0,
+      8: 0,
+      9: 0,
+    };
+
+    const nextState = {
+      0: 1,
+      1: 1,
+      2: 2,
+      3: 1,
+      4: 0,
+      5: 0,
+      6: 0,
+      7: 0,
+      8: 0,
+    };
+    expect(nextPopulation(currentPopulationState)).toStrictEqual(nextState);
+  });
+
+  it("finds the next population state with newborns", () => {
+    const currentPopulationState = {
+      0: 4,
+      1: 3,
+      2: 5,
+      3: 3,
+      4: 2,
+      5: 2,
+      6: 1,
+      7: 1,
+      8: 1,
+    };
+
+    const nextState = {
+      0: 3,
+      1: 5,
+      2: 3,
+      3: 2,
+      4: 2,
+      5: 1,
+      6: 5,
+      7: 1,
+      8: 4,
+    };
+    expect(nextPopulation(currentPopulationState)).toStrictEqual(nextState);
+  });
+
+  it("the solution for 80 days", () => {
+    expect(numberOfFishPart2(input, 80)).toBe(375482);
+  });
+
+  it("the solution for 256 days", () => {
+    expect(numberOfFishPart2(input, 256)).toBe(1689540415957);
   });
 });
--- src/day6/day6.ts
+++ src/day6/day6.ts
@@ -1,10 +1,22 @@
-const numberOfFish = (fishSwarm: number[], days: number): number => {
-  let dailyUpdate = fishSwarm;
+interface FishPopulation {
+  0: number;
+  1: number;
+  2: number;
+  3: number;
+  4: number;
+  5: number;
+  6: number;
+  7: number;
+  8: number;
+}
+
+const numberOfFishPart1 = (fishSwarm: number[], days: number): number => {
+  let dailyState = fishSwarm;
 
   for (let i = 0; i < days; i++) {
-    dailyUpdate = nextState(dailyUpdate);
+    dailyState = nextState(dailyState);
   }
-  return dailyUpdate.length;
+  return dailyState.length;
 };
 
 const nextState = (fishSwarm: number[]): number[] => {
@@ -24,4 +36,49 @@ const nextState = (fishSwarm: number[]): number[] => {
   return [...nextGen, ...newFish];
 };
 
-export { nextState, numberOfFish };
+const numberOfFishPart2 = (input: number[], days: number): number => {
+  const population = convertToPopulation(input);
+
+  let dailyState = population;
+  for (let i = 0; i < days; i++) {
+    dailyState = nextPopulation(dailyState);
+  }
+
+  return Object.values(dailyState).reduce((acc, amount) => acc + amount);
+};
+
+const convertToPopulation = (input: number[]): FishPopulation => {
+  return {
+    0: input.filter((fish) => fish === 0).length,
+    1: input.filter((fish) => fish === 1).length,
+    2: input.filter((fish) => fish === 2).length,
+    3: input.filter((fish) => fish === 3).length,
+    4: input.filter((fish) => fish === 4).length,
+    5: input.filter((fish) => fish === 5).length,
+    6: input.filter((fish) => fish === 6).length,
+    7: input.filter((fish) => fish === 7).length,
+    8: input.filter((fish) => fish === 8).length,
+  };
+};
+
+const nextPopulation = (currentState: FishPopulation): FishPopulation => {
+  return {
+    0: currentState[1],
+    1: currentState[2],
+    2: currentState[3],
+    3: currentState[4],
+    4: currentState[5],
+    5: currentState[6],
+    6: currentState[7] + currentState[0],
+    7: currentState[8],
+    8: currentState[0],
+  };
+};
+
+export {
+  nextState,
+  numberOfFishPart1,
+  nextPopulation,
+  convertToPopulation,
+  numberOfFishPart2,
+};
